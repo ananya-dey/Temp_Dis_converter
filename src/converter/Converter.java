8073b47bad1c8969e5618697709afec188f0b633
anna67 <ananyadey067@gmail.com>
2016-01-07 22:46:01 -0800

    Temperature &  Distance Converter
    Using java

---
+++ src/converter/Converter.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package converter;
+
+/**
+ *
+ * @author Ayon
+ */
+public class Converter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        front a = new front();
+        a.setLocation(300, 200);
+        a.setVisible(true);
+    }
+}
